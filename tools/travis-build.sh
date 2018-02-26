2b0c539ceffe6244ce8d8395d356015bc3ffd21d
DESKTOP-FKABHHG\leanr <lean.randlev@gmail.com>
2018-02-26 16:16:10 +0100

    .

--- tools/travis-build.sh
+++ tools/travis-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	mono ../tools/sonar/SonarQube.Scanner.MSBuild.exe begin /n:sonarcloudtest /k:leandowich-github /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /v:"2.0"
+	mono ./tools/sonar/SonarQube.Scanner.MSBuild.exe begin /n:sonarcloudtest /k:leandowich-github /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /v:"2.0"
 fi
 
 dotnet restore
@@ -11,5 +11,5 @@ dotnet test
 # dotnet test NGenerics.Examples --logger:trx
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	mono ../tools/sonar/SonarQube.Scanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
+	mono ./tools/sonar/SonarQube.Scanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
 fi
