1bb45b376755ffdacc17ef16c686d78c962145db
DESKTOP-FKABHHG\leanr <lean.randlev@gmail.com>
2018-02-26 13:12:44 +0100

    formattet and added abe

--- sonarcloudtest/Controllers/NewController.cs
+++ sonarcloudtest/Controllers/NewController.cs
@@ -1,35 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using System.Numerics;
 
 namespace sonarcloudtest.Controllers
 {
     [Route("api/[controller]")]
     public class NewController : Controller
     {
-          [HttpGet("[action]")]
-                                public IEnumerable<Ietm> Things()
+        [HttpGet("[action]")]
+        public IEnumerable<Ietm> Things()
         {
+            var abe = 2;
+            if (abe == 2)
+            {
+
+            }
+
             var rng = new Random();
-    return Enumerable.Range(1, 5).Select(index => new Ietm
-    {
-        name = "John" + rng.Next()
-    });
+            return Enumerable.Range(1, 5).Select(index => new Ietm
+            {
+                name = "John" + rng.Next()
+            });
         }
 
         public class Ietm
-         {
-                public string name { get; set; }
+        {
+            public string name { get; set; }
 
             public int value
             {
-        get
+                get
                 {
-return 33 * name.Length;
+                    return 33 * name.Length;
                 }
             }
         }
