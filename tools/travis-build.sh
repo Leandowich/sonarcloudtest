32a43906e59d6b1afc532ce31551db3d0ef1dedb
DESKTOP-FKABHHG\leanr <lean.randlev@gmail.com>
2018-02-27 12:53:20 +0100

    .

--- tools/travis-build.sh
+++ tools/travis-build.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	mono ./tools/sonar/SonarQube.Scanner.MSBuild.exe begin /n:sonarcloudtest /k:leandowich-github /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.ts.tslintconfigpath="**/tslint.json" /d:sonar.ts.lcov.reportpath="test-results/coverage/coverage.lcov" /v:"2.0"
 fi
-
+ 
 dotnet restore
 dotnet build
 #dotnet test
